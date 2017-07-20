ec94c7cca25e05352534d384d4443698794494b1
Mark Paluch <mpaluch@pivotal.io>
2017-07-20 10:32:45 +0200

    DATAMONGO-1719 - Polishing.
    Add missing license headers. Use mockito extensions to mimic static imports.

--- spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/ExecutableAggregationOperationExtensionsTests.kt
+++ spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/ExecutableAggregationOperationExtensionsTests.kt
@@ -1,5 +1,21 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.mongodb.core
 
+import com.nhaarman.mockito_kotlin.verify
 import example.first.First
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -10,6 +26,7 @@ import org.mockito.junit.MockitoJUnitRunner
 
 /**
  * @author Sebastien Deleuze
+ * @author Mark Paluch
  */
 @RunWith(MockitoJUnitRunner::class)
 class ExecutableAggregationOperationExtensionsTests {
@@ -19,14 +36,16 @@ class ExecutableAggregationOperationExtensionsTests {
 
 	@Test // DATAMONGO-1689
 	fun `aggregateAndReturn(KClass) extension should call its Java counterpart`() {
+
 		operation.aggregateAndReturn(First::class)
-		Mockito.verify(operation, Mockito.times(1)).aggregateAndReturn(First::class.java)
+		verify(operation).aggregateAndReturn(First::class.java)
 	}
 
 	@Test // DATAMONGO-1689
 	fun `aggregateAndReturn() with reified type parameter extension should call its Java counterpart`() {
+
 		operation.aggregateAndReturn<First>()
-		Mockito.verify(operation, Mockito.times(1)).aggregateAndReturn(First::class.java)
+		verify(operation).aggregateAndReturn(First::class.java)
 	}
 
 }
--- spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/ExecutableFindOperationExtensionsTests.kt
+++ spring-data-mongodb/src/test/kotlin/org/springframework/data/mongodb/core/ExecutableFindOperationExtensionsTests.kt
@@ -1,47 +1,67 @@
+/*
+ * Copyright 2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.mongodb.core
 
+import com.nhaarman.mockito_kotlin.verify
 import example.first.First
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Answers
 import org.mockito.Mock
-import org.mockito.Mockito
 import org.mockito.junit.MockitoJUnitRunner
 
 /**
  * @author Sebastien Deleuze
+ * @author Mark Paluch
  */
 @RunWith(MockitoJUnitRunner::class)
 class ExecutableFindOperationExtensionsTests {
 
-	@Mock(answer = Answers.RETURNS_MOCKS)
-	lateinit var operation: ExecutableFindOperation
+    @Mock(answer = Answers.RETURNS_MOCKS)
+    lateinit var operation: ExecutableFindOperation
 
-	@Mock(answer = Answers.RETURNS_MOCKS)
-	lateinit var operationWithProjection: ExecutableFindOperation.FindOperationWithProjection<First>
+    @Mock(answer = Answers.RETURNS_MOCKS)
+    lateinit var operationWithProjection: ExecutableFindOperation.FindWithProjection<First>
 
-	@Test // DATAMONGO-1689
-	fun `ExecutableFindOperation#query(KClass) extension should call its Java counterpart`() {
-		operation.query(First::class)
-		Mockito.verify(operation, Mockito.times(1)).query(First::class.java)
-	}
+    @Test // DATAMONGO-1689
+    fun `ExecutableFindOperation#query(KClass) extension should call its Java counterpart`() {
 
-	@Test // DATAMONGO-1689
-	fun `ExecutableFindOperation#query() with reified type parameter extension should call its Java counterpart`() {
-		operation.query<First>()
-		Mockito.verify(operation, Mockito.times(1)).query(First::class.java)
-	}
+        operation.query(First::class)
+        verify(operation).query(First::class.java)
+    }
 
-	@Test // DATAMONGO-1689
-	fun `ExecutableFindOperation#FindOperationWithProjection#asType(KClass) extension should call its Java counterpart`() {
-		operationWithProjection.asType(First::class)
-		Mockito.verify(operationWithProjection, Mockito.times(1)).`as`(First::class.java)
-	}
+    @Test // DATAMONGO-1689
+    fun `ExecutableFindOperation#query() with reified type parameter extension should call its Java counterpart`() {
 
-	@Test // DATAMONGO-1689
-	fun `ExecutableFindOperation#FindOperationWithProjection#asType() with reified type parameter extension should call its Java counterpart`() {
-		operationWithProjection.asType()
-		Mockito.verify(operationWithProjection, Mockito.times(1)).`as`(First::class.java)
-	}
+        operation.query<First>()
+        verify(operation).query(First::class.java)
+    }
+
+    @Test // DATAMONGO-1689
+    fun `ExecutableFindOperation#FindOperationWithProjection#asType(KClass) extension should call its Java counterpart`() {
+
+        operationWithProjection.asType(First::class)
+        verify(operationWithProjection).`as`(First::class.java)
+    }
+
+    @Test // DATAMONGO-1689
+    fun `ExecutableFindOperation#FindOperationWithProjection#asType() with reified type parameter extension should call its Java counterpart`() {
+
+        operationWithProjection.asType()
+        verify(operationWithProjection).`as`(First::class.java)
+    }
 
 }
